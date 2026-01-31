71e6346b3e2474f8dd9e5551f993de9fb86bd7bd
Daamin909 <ashai.daamin@gmail.com>
2026-02-01 03:10:19 +0800

    added airplane moving thingy, and death

---
+++ scripts/airplane.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+
+func _physics_process(delta: float) -> void:
+	var y_dir := Input.get_axis("ui_up", "ui_down")
+	velocity.y = y_dir * SPEED
+
+	move_and_slide()
